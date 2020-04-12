3fe73fac4f959c34d2e95c11b2d395f727d4d960
Afridi <Adam.Afridi@iriworldwide.com>
2020-04-11 23:12:03 -0500

    homework

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "398908ea08d23bfe6901389b2792b6e9"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDwEe2nNghKQjELXbK2O1Oo6PaBpybELdI"
